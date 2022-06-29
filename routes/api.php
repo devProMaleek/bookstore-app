9fad76c110fbb1e64963ce58fdf1e807422b2ff7
devProMaleek <milikiadbay@gmail.com>
2022-06-29 21:46:30 +0100

    feat: Made an author endpoints, and learnt about resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,6 @@ Route::middleware('auth:api')->prefix('v1')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::get('authors/{author}', [AuthorsController::class, 'show']);
 });
